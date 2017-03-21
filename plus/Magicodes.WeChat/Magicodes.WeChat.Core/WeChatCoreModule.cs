2aac0722144aa8734b782c4835965915e07c2193
李文强 <codelove1314@live.cn>
2017-03-21 19:59:57 +0800

    添加Magicodes.Data

---
+++ plus/Magicodes.WeChat/Magicodes.WeChat.Core/WeChatCoreModule.cs
@@ -0,0 +1,29 @@
+﻿using Abp.AutoMapper;
+using Abp.Modules;
+using Abp.Zero;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magicodes.WeChat.Core
+{
+    [DependsOn(
+       typeof(AbpZeroCoreModule),
+       typeof(AbpAutoMapperModule))]
+    public class WeChatCoreModule: AbpModule
+    {
+        public override void PreInitialize()
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
---
+++ plus/Magicodes.WeChat/Magicodes.WeChat.Data/WeChatDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Abp.EntityFramework;
+using Magicodes.Admin.EntityFramework;
+using Magicodes.WeChat.Core.User;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magicodes.WeChat.Data
+{
+    [DbConfigurationType(typeof(WeChatDbConfiguration))]
+    public class WeChatDbContext : AbpDbContext
+    {
+        public virtual IDbSet<User_WeChatUser> User_WeChatUsers { get; set; }
+
+        public WeChatDbContext() : base()
+        {
+        }
+
+        public WeChatDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+
+        }
+
+        public WeChatDbContext(DbConnection existingConnection)
+            : base(existingConnection, false)
+        {
+
+        }
+
+        public WeChatDbContext(DbConnection existingConnection, bool contextOwnsConnection)
+            : base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+
+    }
+    public class WeChatDbConfiguration : DbConfiguration
+    {
+        public WeChatDbConfiguration()
+        {
+            SetProviderServices(
+                "System.Data.SqlClient",
+                System.Data.Entity.SqlServer.SqlProviderServices.Instance
+            );
+        }
+    }
+}
