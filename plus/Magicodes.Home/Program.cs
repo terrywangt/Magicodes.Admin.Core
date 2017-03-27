594f3c893ef3b2295bcc088589604a5c2e3f84e6
李文强 <codelove1314@live.cn>
2017-03-27 17:28:29 +0800

    完善插件

---
+++ plus/Magicodes.Home/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Magicodes.Home
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ plus/Magicodes.WeChat/Magicodes.WeChat.Web.Mvc/WeChatWebModule.cs
@@ -0,0 +1,44 @@
+﻿using Abp.Configuration;
+using Abp.Modules;
+using Abp.Resources.Embedded;
+using Magicodes.Admin.Configuration;
+using Magicodes.Home.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Magicodes.Admin.Configuration.Host;
+using Abp.AutoMapper;
+using Abp.Modules;
+using Magicodes.Admin;
+using Magicodes.Admin.Authorization;
+using Abp.Authorization;
+
+namespace Magicodes.WeChat.Web.Mvc
+{
+    [DependsOn(
+        typeof(AdminCoreModule),
+        typeof(AbpAutoMapperModule),
+        typeof(AdminApplicationModule))]
+    public class HomeModule : AbpModule
+    {
+
+        public override void PreInitialize()
+        {
+        }
+
+        public override void PostInitialize()
+        {
+            
+        }
+
+        public override void Initialize()
+        {
+            ////添加导航
+            //Configuration.Navigation.Providers.Add<MetronicNavigationProvider>();
+
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
