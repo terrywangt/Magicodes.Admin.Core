8d6ea6af791866a9891c656273ab583af5644160
Dai Mr <1822802785@qq.com>
2017-10-20 17:25:43 +0800

    修改sql问题

--- src/Magicodes.Admin.EntityFrameworkCore/EntityFrameworkCore/AdminDbContextConfigurer.cs
+++ src/Magicodes.Admin.EntityFrameworkCore/EntityFrameworkCore/AdminDbContextConfigurer.cs
@@ -7,18 +7,15 @@ namespace Magicodes.Admin.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<AdminDbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            //builder.UseSqlServer(connectionString);
             //var configuration = AppConfigurations.Get(WebContentDirectoryFinder.CalculateContentRootFolder());
-            ////以支持SQL Server 2012以下数据库
-            //if (configuration.GetValue<bool>("UseRowNumberForPaging"))
-            //    builder.UseSqlServer(connectionString, p => p.UseRowNumberForPaging());
-            //else
-            //    builder.UseSqlServer(connectionString);
+            //以支持SQL Server 2012以下数据库
+            builder.UseSqlServer(connectionString, p => p.UseRowNumberForPaging());
         }
 
-        public static void Configure(DbContextOptionsBuilder<AdminDbContext> builder, DbConnection connection)
-        {
-            builder.UseSqlServer(connection);
-        }
+        //public static void Configure(DbContextOptionsBuilder<AdminDbContext> builder, DbConnection connection)
+        //{
+        //    builder.UseSqlServer(connection);
+        //}
     }
 }
