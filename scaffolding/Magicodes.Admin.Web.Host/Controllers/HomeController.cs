dd3f0f039d9160e482780efdedc7547bb027f2fe
李文强 <codelove1314@live.cn>
2017-11-09 17:10:14 +0800

    添加两个项目模板

---
+++ scaffolding/Magicodes.Admin.Web.Host/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Auditing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magicodes.Admin.Web.Controllers
+{
+    public class HomeController : AdminControllerBase
+    {
+        [DisableAuditing]
+        public IActionResult Index()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
---
+++ scaffolding/Magicodes.Admin.Web.Host/Url/WebUrlService.cs
@@ -0,0 +1,19 @@
+using Abp.Dependency;
+using Magicodes.Admin.Configuration;
+using Magicodes.Admin.Url;
+
+namespace Magicodes.Admin.Web.Url
+{
+    public class WebUrlService : WebUrlServiceBase, IWebUrlService, ITransientDependency
+    {
+        public WebUrlService(
+            IAppConfigurationAccessor configurationAccessor) :
+            base(configurationAccessor)
+        {
+        }
+
+        public override string WebSiteRootAddressFormatKey => "App:ClientRootAddress";
+
+        public override string ServerRootAddressFormatKey => "App:ServerRootAddress";
+    }
+}
