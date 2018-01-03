7a4c314abb5df4290b93bb14ab38474b0d906f78
李文强 <codelove1314@live.cn>
2018-01-03 15:07:03 +0800

    添加Angular版本

---
+++ angular/e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+﻿import { AdminPage } from './app.po';
+
+describe('abp-zero-template App', function () {
+    let page: AdminPage;
+
+    beforeEach(() => {
+        page = new AdminPage();
+    });
+
+    it('should display message saying app works', () => {
+        page.navigateTo();
+        page.getCopyright().then(value => {
+            expect(value).toEqual(new Date().getFullYear() + ' © Admin.');
+        });
+    });
+});
