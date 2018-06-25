a54c72209aa44bd87c40b3be7638e794f1659e2b
李文强 <codelove1314@live.cn>
2018-06-25 18:18:34 +0800

    修改语言定义 添加SwitchEntityInputDto

---
+++ src/Magicodes.Admin.Application.Shared/Dto/SwitchEntityInputDto.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magicodes.Admin.Dto
+{
+    /// <summary>
+    /// 开关输入参数Dto  
+    /// </summary>
+    /// <typeparam name="TPrimaryKey"></typeparam>
+    public class SwitchEntityInputDto<TPrimaryKey>: EntityDto<TPrimaryKey>
+    {
+        /// <summary>
+        /// 开关值（bool类型）
+        /// </summary>
+        public bool SwitchValue { get; set; }
+    }
+}
