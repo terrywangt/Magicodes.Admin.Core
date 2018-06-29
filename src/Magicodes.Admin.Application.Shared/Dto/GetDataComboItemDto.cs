1792001bdb8d444bdd64f2c2c762090e2b053b32
李文强 <codelove1314@live.cn>
2018-06-29 16:25:12 +0800

    添加GetDataComboItemDto通用Dto，以支持对关联项的代码生成

---
+++ src/Magicodes.Admin.Application.Shared/Dto/GetDataComboItemDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magicodes.Admin.Dto
+{
+    /// <summary>
+    /// 获取下拉列表
+    /// </summary>
+    /// <typeparam name="TPrimaryKey"></typeparam>
+    public class GetDataComboItemDto<TPrimaryKey>
+    {
+        /// <summary>
+        /// 显示名（会进行语言处理）
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        public TPrimaryKey Value { get; set; }
+    }
+}
