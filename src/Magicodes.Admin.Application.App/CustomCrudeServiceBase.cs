9a14877a5f9dfa909ba7aff79449a195889053bb
李文强 <codelove1314@live.cn>
2018-07-11 17:50:45 +0800

    添加两个基类： 自定义增删查改恢复导出服务基类 自定义增删查改导出服务基类

---
+++ src/Magicodes.Admin.Application.App/CustomCrudeServiceBase.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.AspNetZeroCore.Net;
+using Abp.Authorization;
+using Abp.AutoMapper;
+using Abp.Domain.Entities;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Abp.Extensions;
+using Abp.UI;
+using Magicodes.Admin.Dto;
+using Magicodes.ExporterAndImporter.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Magicodes.Admin.Application.App
+{
+    /// <summary>
+    /// 自定义增删查改导出服务基类
+    /// 增
+    /// 删
+    /// 改
+    /// 查
+    /// 导出
+    /// </summary>
+    /// <typeparam name="TEntity">实体模型</typeparam>
+    /// <typeparam name="TEntityDto">实体列表Dto</typeparam>
+    /// <typeparam name="TPrimaryKey">主键</typeparam>
+    /// <typeparam name="TGetAllInput">查询所有Input</typeparam>
+    /// <typeparam name="TCreateInput">创建Input</typeparam>
+    /// <typeparam name="TUpdateInput">修改Input</typeparam>
+    /// <typeparam name="TExportDto">导出Dto</typeparam>
+    [AbpAuthorize]
+    public abstract class CustomCrudeServiceBase<TEntity, TEntityDto, TPrimaryKey, TGetAllInput, TCreateInput, TUpdateInput, TExportDto> : AsyncCrudAppService<TEntity, TEntityDto, TPrimaryKey, TGetAllInput, TCreateInput, TUpdateInput>
+        where TEntity : class, IEntity<TPrimaryKey>
+        where TEntityDto : IEntityDto<TPrimaryKey>
+        where TCreateInput : IEntityDto<TPrimaryKey>
+        where TUpdateInput : IEntityDto<TPrimaryKey>
+        where TGetAllInput : IPagedAndSortedResultRequest
+        where TExportDto : class 
+
+    {
+        /// <summary>
+        /// 目录
+        /// </summary>
+        public IAppFolders AppFolders { get; set; }
+
+        /// <summary>
+        /// 恢复权限
+        /// </summary>
+        public  string RestorePermissionName { get; set; }
+
+        /// <summary>
+        /// 导出权限
+        /// </summary>
+        public string ExportPermissionName { get; set; }
+
+        /// <summary>
+        /// Excel导出注入
+        /// </summary>
+        public IExporter ExcelExporter { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repository"></param>
+        protected CustomCrudeServiceBase(IRepository<TEntity, TPrimaryKey> repository) : base(repository)
+        {
+        }
+
+        /// <summary>
+        /// 获取临时目录路径
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="ext"></param>
+        /// <returns></returns>
+        protected string GetTempFilePath(string fileName = null, string ext = null)
+        {
+            if (fileName.IsNullOrEmpty())
+            {
+                fileName = Guid.NewGuid().ToString("N");
+            }
+            return Path.Combine(AppFolders.TempFileDownloadFolder, $"{fileName}{ext}");
+        }
+
+        /// <summary>
+        /// 导出Excel
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public virtual async Task<FileDto> ToExcel(TGetAllInput input)
+        {
+            CheckPermission(ExportPermissionName);
+            List<TExportDto> exportData = null;
+            using (UnitOfWorkManager.Current.DisableFilter(AbpDataFilters.SoftDelete))
+            {
+                var query = CreateFilteredQuery(input);
+                var results = await query
+                    .OrderBy(input.Sorting)
+                    .ToListAsync();
+
+                exportData = results.MapTo<List<TExportDto>>();
+                if (exportData.Count == 0)
+                    throw new UserFriendlyException(L("NoDataToExport"));
+            }
+            var fileDto = new FileDto(L(typeof(TEntity).Name) + L("ExportData") + ".xlsx", MimeTypeNames.ApplicationVndOpenxmlformatsOfficedocumentSpreadsheetmlSheet);
+            var filePath = GetTempFilePath(fileName: fileDto.FileToken);
+            await ExcelExporter.Export(filePath, exportData);
+            return fileDto;
+        }
+    }
+}
---
+++ src/Magicodes.Admin.Application.App/CustomCruderServiceBase.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using Abp.Authorization;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Abp.Runtime.Session;
+using Abp.Timing;
+
+namespace Magicodes.Admin.Application.App
+{
+    /// <summary>
+    /// 自定义增删查改恢复导出服务基类
+    /// 增
+    /// 删
+    /// 改
+    /// 查
+    /// 导出
+    /// 恢复
+    /// </summary>
+    /// <typeparam name="TEntity">实体模型</typeparam>
+    /// <typeparam name="TEntityDto">实体列表Dto</typeparam>
+    /// <typeparam name="TPrimaryKey">主键</typeparam>
+    /// <typeparam name="TGetAllInput">查询所有Input</typeparam>
+    /// <typeparam name="TCreateInput">创建Input</typeparam>
+    /// <typeparam name="TUpdateInput">修改Input</typeparam>
+    /// <typeparam name="TExportDto">导出Dto</typeparam>
+    [AbpAuthorize]
+    public abstract class CustomCruderServiceBase<TEntity, TEntityDto, TPrimaryKey, TGetAllInput, TCreateInput, TUpdateInput, TExportDto> : CustomCrudeServiceBase<TEntity, TEntityDto, TPrimaryKey, TGetAllInput, TCreateInput, TUpdateInput, TExportDto>
+        where TEntity : class, IEntity<TPrimaryKey>
+        where TEntityDto : IEntityDto<TPrimaryKey>
+        where TCreateInput : IEntityDto<TPrimaryKey>
+        where TUpdateInput : IEntityDto<TPrimaryKey>
+        where TGetAllInput : IPagedAndSortedResultRequest
+        where TExportDto : class 
+
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repository"></param>
+        protected CustomCruderServiceBase(IRepository<TEntity, TPrimaryKey> repository) : base(repository)
+        {
+        }
+
+        /// <summary>
+        /// 查询所有
+        /// 默认关闭软删除筛选器以支持代码生成时的回收站服务
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public override async Task<PagedResultDto<TEntityDto>> GetAll(TGetAllInput input)
+        {
+            using (UnitOfWorkManager.Current.DisableFilter(AbpDataFilters.SoftDelete))
+            {
+                return await base.GetAll(input);
+            }
+        }
+
+        /// <summary>
+        /// 恢复当前数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public virtual async Task Restore(TPrimaryKey id)
+        {
+            CheckPermission(RestorePermissionName);
+            using (UnitOfWorkManager.Current.DisableFilter(AbpDataFilters.SoftDelete))
+            {
+                var model = await Repository.GetAsync(id);
+                if (model is ISoftDelete delete)
+                {
+                    delete.IsDeleted = false;
+                }
+                if (model is IModificationAudited moduiAudited)
+                {
+                    moduiAudited.LastModifierUserId = AbpSession.GetUserId();
+                    moduiAudited.LastModificationTime = Clock.Now;
+                }
+            }
+        }
+    }
+}
