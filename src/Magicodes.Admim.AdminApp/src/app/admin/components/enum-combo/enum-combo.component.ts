f440b05c20af757c63ea3bcaa153f6ad899e4cb1
张吉贵 <stoning.code@gmail.com>
2018-07-12 17:59:47 +0800

    添加angular通用组件(审计日志，枚举，文件上传，图片列表，下拉框组件)

---
+++ src/Magicodes.Admim.AdminApp/src/app/admin/components/enum-combo/enum-combo.component.ts
@@ -0,0 +1,45 @@
+import { Component, ElementRef, EventEmitter, Injector, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import { GetEnumValuesListDto, CommonServiceProxy } from '@shared/service-proxies/service-proxies';
+
+@Component({
+    selector: 'enum-combo',
+    template:
+        `<select #EnumCombobox
+        class="form-control"
+        [(ngModel)]="selectedValue"
+        (ngModelChange)="selectedValueChange.emit($event)"
+        [attr.data-live-search]="true"
+        jq-plugin="selectpicker">
+            <option value="">{{emptyText}}</option>
+            <option *ngFor="let item of values" [value]="item.value">{{item.displayName}}</option>
+    </select>`
+})
+export class EnumComboComponent extends AppComponentBase implements OnInit {
+    @ViewChild('EnumCombobox') EnumComboboxElement: ElementRef;
+    //选择的值
+    @Input() selectedValue: number = null;
+    @Output() selectedValueChange: EventEmitter<string> = new EventEmitter<string>();
+    //空文本提示
+    @Input() emptyText = this.l('EmptyTextTip');
+    //枚举类型全名
+    @Input() fullName = '';
+
+    values: GetEnumValuesListDto[] = [];
+
+    constructor(
+        private _commonService: CommonServiceProxy,
+        injector: Injector) {
+        super(injector);
+    }
+
+    ngOnInit(): void {
+        const self = this;
+        this._commonService.getEnumValuesList(this.fullName).subscribe(result => {
+            this.values = result;
+            setTimeout(() => {
+                $(self.EnumComboboxElement.nativeElement).selectpicker('refresh');
+            }, 0);
+        });
+    }
+}
