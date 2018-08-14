d4b1ff25e5f64631595925fd421b9d3610da2bec
张吉贵 <stoning.code@gmail.com>
2018-08-14 10:10:41 +0800

    完善core引用，添加获取树节点Dto

---
+++ src/application/Application.Shared/Dto/GetTreeNodesInputDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magicodes.Admin.Dto
+{
+    public class GetTreeNodesInputDto
+    {
+        public long? ParentId { get; set; }
+    }
+}
