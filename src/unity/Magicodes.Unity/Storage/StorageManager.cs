7b2a3e92bd1cb62113bf641c65651646d7aff204
代瓒 <1822802785@qq.com>
2018-09-04 14:06:57 +0800

    添加阿里云存储

--- src/unity/Magicodes.Unity/Storage/StorageManager.cs
+++ src/unity/Magicodes.Unity/Storage/StorageManager.cs
@@ -1,5 +1,8 @@
 ﻿using System.IO;
+using Abp.Extensions;
+using Abp.UI;
 using Magicodes.Admin.Configuration;
+using Magicodes.Storage.AliyunOss.Core;
 using Magicodes.Storage.Core;
 using Magicodes.Storage.Local.Core;
 using Microsoft.AspNetCore.Hosting;
@@ -31,19 +34,49 @@ namespace Magicodes.Unity.Storage
             switch (_appConfiguration.Configuration["StorageProvider:Type"])
             {
                 case "LocalStorageProvider":
-                {
-                    var rootPath = _appConfiguration.Configuration["StorageProvider:LocalStorageProvider:RootPath"];
-                    if (!rootPath.Contains(":"))
                     {
-                        rootPath = Path.Combine(_env.WebRootPath, rootPath);
+                        var rootPath = _appConfiguration.Configuration["StorageProvider:LocalStorageProvider:RootPath"];
+                        if (!rootPath.Contains(":"))
+                        {
+                            rootPath = Path.Combine(_env.WebRootPath, rootPath);
+                        }
+
+                        if (!Directory.Exists(rootPath)) Directory.CreateDirectory(rootPath);
+
+                        StorageProvider = new LocalStorageProvider(rootPath, _appConfiguration.Configuration["StorageProvider:LocalStorageProvider:RootUrl"]);
+                        break;
                     }
+                case "AliyunOssStorageProvider":
+                    {
+                        var accessKeyId =
+                            _appConfiguration.Configuration["StorageProvider:AliyunOssStorageProvider:AccessKeyId"];
+                        var accessKeySecret =
+                            _appConfiguration.Configuration["StorageProvider:AliyunOssStorageProvider:AccessKeySecret"];
+                        var endpoint =
+                            _appConfiguration.Configuration["StorageProvider:AliyunOssStorageProvider:Endpoint"];
+                        if (accessKeyId.IsNullOrWhiteSpace())
+                        {
+                            throw new UserFriendlyException("AliyunOssStorageProvider accessKeyId is null!");
+                        }
+                        if (accessKeySecret.IsNullOrWhiteSpace())
+                        {
+                            throw new UserFriendlyException("AliyunOssStorageProvider accessKeySecret is null!");
+                        }
+                        if (endpoint.IsNullOrWhiteSpace())
+                        {
+                            throw new UserFriendlyException("AliyunOssStorageProvider endpoint is null!");
+                        }
 
-                    if (!Directory.Exists(rootPath)) Directory.CreateDirectory(rootPath);
-
-                    StorageProvider = new LocalStorageProvider(rootPath, _appConfiguration.Configuration["StorageProvider:LocalStorageProvider:RootUrl"]);
-                    break;
-                }
-                //TODO:阿里云、腾讯云支持
+                        var aliyunOssConfig = new AliyunOssConfig
+                        {
+                            AccessKeyId = accessKeyId,
+                            AccessKeySecret = accessKeySecret,
+                            Endpoint = endpoint
+                        };
+                        StorageProvider = new AliyunOssStorageProvider(aliyunOssConfig);
+                        break;
+                    }
+                //TODO:腾讯云支持
                 default:
                     break;
             }
