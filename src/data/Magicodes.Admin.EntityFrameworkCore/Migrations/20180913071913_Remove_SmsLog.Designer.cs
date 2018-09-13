ea85459a78804b07e631d836f0a4693877191d88
李文强 <codelove1314@live.cn>
2018-09-13 15:20:15 +0800

    移除短信日志表

--- src/data/Magicodes.Admin.EntityFrameworkCore/Migrations/20180913071913_Remove_SmsLog.Designer.cs
+++ src/data/Magicodes.Admin.EntityFrameworkCore/Migrations/20180913071913_Remove_SmsLog.Designer.cs
@@ -4,14 +4,16 @@ using Magicodes.Admin.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Magicodes.Admin.Migrations
 {
     [DbContext(typeof(AdminDbContext))]
-    partial class AdminDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180913071913_Remove_SmsLog")]
+    partial class Remove_SmsLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
