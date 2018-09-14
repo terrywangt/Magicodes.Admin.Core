06cdc62b139bc83d2d19e043d658c4da7daa1917
李文强 <codelove1314@live.cn>
2018-09-14 11:24:39 +0800

    修复验证码单元测试

--- src/core/Magicodes.Admin.Core/Identity/Cache/SmsVerificationCodeCacheItem.cs
+++ src/core/Magicodes.Admin.Core/Identity/Cache/SmsVerificationCodeCacheItem.cs
@@ -19,7 +19,7 @@ namespace Magicodes.Admin.Identity.Cache
         /// <summary>
         /// 创建时间
         /// </summary>
-        public DateTime CreationTime { get; set; }
+        public DateTime CreationTime { get; set; } = Clock.Now;
 
         /// <summary>
         /// 过期时间
@@ -31,7 +31,6 @@ namespace Magicodes.Admin.Identity.Cache
         /// </summary>
         public SmsVerificationCodeCacheItem()
         {
-
         }
 
         /// <summary>
@@ -41,7 +40,6 @@ namespace Magicodes.Admin.Identity.Cache
         public SmsVerificationCodeCacheItem(string code)
         {
             Code = code;
-            CreationTime = Clock.Now;
         }
     }
 }
