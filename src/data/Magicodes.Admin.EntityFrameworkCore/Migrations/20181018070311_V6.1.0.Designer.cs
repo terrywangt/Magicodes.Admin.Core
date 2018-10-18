90cf3f6e0315d133250359c431c9927dcd872936
李文强 <codelove1314@live.cn>
2018-10-18 15:03:38 +0800

    添加数据迁移

--- src/data/Magicodes.Admin.EntityFrameworkCore/Migrations/20181018070311_V6.1.0.Designer.cs
+++ src/data/Magicodes.Admin.EntityFrameworkCore/Migrations/20181018070311_V6.1.0.Designer.cs
@@ -4,14 +4,16 @@ using Magicodes.Admin.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Magicodes.Admin.Migrations
 {
     [DbContext(typeof(AdminDbContext))]
-    partial class AdminDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181018070311_V6.1.0")]
+    partial class V610
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
