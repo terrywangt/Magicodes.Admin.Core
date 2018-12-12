9483ac2da524cfd0079f09ad0f76d7728690f84a
代瓒 <1822802785@qq.com>
2018-12-12 17:35:47 +0800

    删除App UI框架

--- src/app/ui/App/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  enableProd:true,
-  domain:'http://xxxxxx.com',
-};
--- src/app/ui/App/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  enableProd:true,
-  domain:'http://xxxxxx.com',
-  token:''
-};
